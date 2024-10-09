b2949acaed3fff822b962c0e2d7cf104df8ec673
rishabhramani <2023.rishabh.ramani@ves.ac.in>
2024-10-09 12:08:03 +0530

    Update jdk.java

--- jdk.java
+++ jdk.java
@@ -1,5 +1,19 @@
-public class jdk{
-  public static void main(string[]args){
-    System.out.println("HELLO VESIT");
-  }
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        double num1 = scanner.nextDouble();
+        System.out.print("Enter second number: ");
+        double num2 = scanner.nextDouble();
+        
+        double sum = num1 + num2;
+        double difference = num1 - num2;
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+        
+        scanner.close();
+    }
 }
